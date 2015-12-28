e9ddb1da241009a17b01b09615697a8386d9ec2a
Ian Behrmann <ianjbehrmann@gmail.com>
2015-12-28 15:54:45 -0700

    working search including depth

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var knex = require('knex')(require('../knexfile')['development']);
+var createAdjacencyList = require('../lib/filterConnections').adjacencyList
+var findConnections = require('../lib/filterConnections').findConnections
 
 router.get('/', function(req, res, next) {
   knex('users').then(function (users) {
@@ -11,20 +13,23 @@ router.get('/', function(req, res, next) {
 router.get('/users/:id', function(req, res, next) {
   knex('users').where({id: req.params.id}).first().then(function (user) {
 
+    // actual raw sql query
+    // select user_id, users.name as user_name, other_id, others.name as other_name
+    // from connections
+    // inner join users on users.id = connections.user_id
+    // inner join users others on others.id = connections.other_id;
+
     knex('connections')
-      .where({user_id: user.id})
-      .innerJoin('users', 'other_id', 'users.id')
+      .select('user_id', 'users.name', 'other_id', 'others.name as other_name')
+      .innerJoin('users', 'user_id', 'users.id')
+      .innerJoin('users as others', 'other_id', 'others.id')
       .then(function (connections) {
+        var list = createAdjacencyList(connections)
+        var results = findConnections(list, req.params.id)
 
-        knex('connections')
-          .where({other_id: user.id})
-          .innerJoin('users', 'user_id', 'users.id')
-          .then(function (connections2) {
+        // var friends = searchGraph(connections)
 
-
-
-            res.render('user', { user: user, connections: connections.concat(connections2) });
-        })
+        res.render('user', { user: user, list: list, results: results });
     })
 
   })
