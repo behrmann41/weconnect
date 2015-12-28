9203ea3b12b39d51fea128aa149bd5f25250318f
Ian Behrmann <ianjbehrmann@gmail.com>
2015-12-28 12:04:20 -0700

    connections table created with seed data

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,15 @@ var knex = require('knex')(require('../knexfile')['development']);
 
 router.get('/', function(req, res, next) {
   knex('users').then(function (users) {
-    res.render('index', { title: 'Express', users: users });
+    res.render('index', { users: users });
   })
 });
+
+router.get('/users/:id', function(req, res, next) {
+  knex('users').where({id: req.params.id}).first().then(function (user) {
+    res.render('user', { user: user });
+  })
+});
+
 
 module.exports = router;
